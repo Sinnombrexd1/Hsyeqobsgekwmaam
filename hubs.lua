98309af8ce8752da7cc95446148b88554e950df3
Sinnombrexd1 <jorgeromancamacho13@gmail.com>
2025-01-18 14:52:11 -0500

    Add files via upload

---
+++ hubs.lua
@@ -0,0 +1,43 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/Le0Aq8B7"))()
+local Window = Library.CreateLib("​​​​​CapybaraScript Hub", colors)
+
+local colors = {
+    SchemeColor = Color3.fromRGB(255, 87, 34),
+    Background = Color3.fromRGB(36, 36, 36),
+    Header = Color3.fromRGB(54, 54, 54),
+    TextColor = Color3.fromRGB(255, 255, 255),
+    ElementColor = Color3.fromRGB(121, 85, 72)
+}
+local MICTab = Window:NewTab("Info")
+local CombateTab = Window:NewTab("Murder Mystery 2")
+local HitboxTab = Window:NewTab("Duelo Sheriff Vs Murderer")
+local VisualTab = Window:NewTab("Stealthy Aimbot")
+local BTab = Window:NewTab("Be a Npc Or die")
+local xdTab = Window:NewTab("MM2 Aim Trainer")
+
+local MICSection = MICTab:NewSection("Opciones INFO")
+local CombateSection = CombateTab:NewSection("Opciones MM2")
+local HitboxSection = HitboxTab:NewSection("Opciones Duelo Sheriff Vs Murderer")
+local VisualSection = VisualTab:NewSection("Opciones Stealthy Aimbot")
+local BSection = BTab:NewSection("Opciones Be a Npc Or die")
+local xdSection = xdTab:NewSection("Opciones MM2 Aim Trainer")
+
+CombateSection:NewButton("Murder Mystery 2", "ButtonInfo", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jorgelinea/Ahj/refs/heads/main/Protected_5227260397926876.txt"))()
+end)
+CombateSection:NewLabel("I recommend the one below")
+CombateSection:NewButton("Murder Mystery 2"does not have a menu"", "has everything you need", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sinnombrexd1/Lolol/refs/heads/main/%F0%9F%92%A6%F0%9F%97%A3%EF%B8%8F"))()
+end)
+HitboxSection:NewButton("Duelo Sheriff Vs Murderer", "ButtonInfo", function()
+loadstring(game:HttpGet("https://pastebin.com/raw/KSEEgARB"))()
+end)
+VisualSection:NewButton("Stealthy Aimbot", "ButtonInfo", function()
+loadstring(game:HttpGet("https://pastebin.com/raw/kT3sxuBY"))()
+end)
+BSection:NewButton("Be a Npc Or die", "ButtonInfo", function()
+loadstring(game:HttpGet("https://pastebin.com/raw/kxbdiNsq"))()
+end)
+xdSection:NewButton("MM2 Aim Trainer", "ButtonInfo", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jorgelinea/Bysgisb/refs/heads/main/Protected_4742543876396808.txt", true))()
+end)
